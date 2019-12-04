366cffddddde2976c58d03b07e8fdc0859a4604b
jherna <jherna76@elpuig.xeill.net>
2019-12-04 17:16:42 +0100

    1st commit

---
+++ ActivitatExceptions/Control/OperacionsBanc.java
@@ -0,0 +1,16 @@
+package excepcions.ActivitatExceptions.Control;
+
+import excepcions.ActivitatExceptions.Model.CompteEstalvi;
+
+public class OperacionsBanc {
+
+
+    public static boolean verifyDNI(String dni) {
+        //TODO implementar fòrnula de verificació del DNI
+        return true;
+    }
+
+    public static void transferencia(CompteEstalvi font, CompteEstalvi desti, double suma) {
+        //TODO implementar transferència
+    }
+}
